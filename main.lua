821c383cbe40ad30be6126a39c14019e1c341e86
mominahmed99 <36240661+mominahmed99@users.noreply.github.com>
2018-02-13 13:26:25 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,11 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- created by momin ahmed
+-- created on 8th Feb 2018
+--
+-- shows an image oon the iPad and changes background color
+-----------------------------------------------------------------------------------------
+display.setDefault( "background", 1, 1, 0 )
+local image = display.newImage( "./assets/sprites/Dead.png", 100, 100 )
